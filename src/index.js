9f9a1a01aa4c0d65651ce8f74a387795514369e8
emmaodia <odia.emma@gmail.com>
2020-11-06 03:56:36 +0100

    UPDATE: Loaded App Recipes in INdex.js file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/main.css';
+import './styles/global.css';import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
